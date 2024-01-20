bb6d08c788b36a48fa23f570b39db8e65a6fd7d1
iCodePirate <ajjim566@gmail.com>
2024-01-20 13:46:54 +0600

    3 round image add to the asset derectory and the including files

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,13 +1,14 @@
 class TImages{
 // //App Logos
-  static const String darkAppLogo=
-
-      "assets/logos/t-store-splash-logo-black.png";
+  static const String darkAppLogo= "assets/logos/t-store-splash-logo-black.png";
   static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
 // //Social Logos
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 
-
+//Banners
+  static const String promoBanner1 = "assets/images/banners/promo_bannar01.png";
+  static const String promoBanner2 = "assets/images/banners/promo_bannar02.png";
+  static const String promoBanner3 = "assets/images/banners/promo_bannar03.png";
 
 }
